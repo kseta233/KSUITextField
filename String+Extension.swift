9648b35c4417fd7186c0c279c6e494b7f89341a5
kseta233 <kusuma.seta@gmail.com>
2017-09-01 22:37:47 +0700

    Update String+Extension.swift

--- String+Extension.swift
+++ String+Extension.swift
@@ -1,9 +1,8 @@
 //
 //  String+Extension.swift
-//  MassKredit
 //
 //  Created by Kusuma Seta on 8/22/17.
-//  Copyright © 2017 com.pacificaraya.masskredit. All rights reserved.
+//  Copyright © 2017 All rights reserved.
 //
 
 import Foundation
@@ -30,105 +29,5 @@ extension String {
         } catch {
             return false
         }
-    }
-    
-    var isNotEmpty: Bool {
-        return (self.characters.count == 0) ? false : true
-    }
-    
-    var isValidDateFormat:Bool {
-        return true
-    }
-    
-    
-    var length: Int {
-        return self.characters.count
-    }
-    
-    subscript (i: Int) -> String {
-        return self[Range(i ..< i + 1)]
-    }
-    
-    func substring(from: Int) -> String {
-        return self[Range(min(from, length) ..< length)]
-    }
-    
-    func substring(to: Int) -> String {
-        return self[Range(0 ..< max(0, to))]
-    }
-    
-    subscript (r: Range<Int>) -> String {
-        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
-                                            upper: min(length, max(0, r.upperBound))))
-        let start = index(startIndex, offsetBy: range.lowerBound)
-        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
-        return self[Range(start ..< end)]
-    }
-    
-    func substring(from: Int?, to: Int?) -> String {
-        if let start = from {
-            guard start < self.characters.count else {
-                return ""
-            }
-        }
-        
-        if let end = to {
-            guard end >= 0 else {
-                return ""
-            }
-        }
-        
-        if let start = from, let end = to {
-            guard end - start >= 0 else {
-                return ""
-            }
-        }
-        
-        let startIndex: String.Index
-        if let start = from, start >= 0 {
-            startIndex = self.index(self.startIndex, offsetBy: start)
-        } else {
-            startIndex = self.startIndex
-        }
-        
-        let endIndex: String.Index
-        if let end = to, end >= 0, end < self.characters.count {
-            endIndex = self.index(self.startIndex, offsetBy: end + 1)
-        } else {
-            endIndex = self.endIndex
-        }
-        
-        return self[startIndex ..< endIndex]
-    }
-    
-    func substring(from: Int?, length: Int) -> String {
-        guard length > 0 else {
-            return ""
-        }
-        
-        let end: Int
-        if let start = from, start > 0 {
-            end = start + length - 1
-        } else {
-            end = length - 1
-        }
-        
-        return self.substring(from: from, to: end)
-    }
-    
-    func substring(length: Int, to: Int?) -> String {
-        guard let end = to, end > 0, length > 0 else {
-            return ""
-        }
-        
-        let start: Int
-        if let end = to, end - length > 0 {
-            start = end - length + 1
-        } else {
-            start = 0
-        }
-        
-        return self.substring(from: start, to: to)
-    }
-    
+    }    
 }
